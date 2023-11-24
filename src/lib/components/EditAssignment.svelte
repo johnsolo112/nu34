8de2195eaf4feff52ebb3571025443f3b6503ed1
John <115387709+johnsolo112@users.noreply.github.com>
2023-11-24 14:19:02 -0800

    Add files via upload

---
+++ src/lib/components/EditAssignment.svelte
@@ -0,0 +1,160 @@
+<script>
+	import { session } from '$app/stores'
+	import { fly } from 'svelte/transition'
+	import { fade } from 'svelte/transition'
+	import { parseData } from '$lib/js/parseData.js'
+
+	export let course
+
+	let assignment, index
+
+	let shown = false
+	let error = ''
+	let points = null
+	let total = null
+
+	export function show(index) {
+		index = index
+		assignment = course.Marks.Mark.Assignments.Assignment[index]
+		clear()
+		shown = true
+	}
+
+	function clear() {
+		error = ''
+		points = assignment.points
+		total = assignment.total
+	}
+
+	function reset() {
+		error = ''
+		points = assignment.pointsOriginal
+		total = assignment.totalOriginal
+	}
+
+	function del() {
+		shown = false
+		course.Marks.Mark.Assignments.Assignment.splice(assignment.index, 1)
+		parseData($session, null)
+		$session = $session
+	}
+
+	function cancel() {
+		shown = false
+	}
+
+	function save() {
+		if (points == null && total) {
+			error = 'Please enter a point value.'
+			return
+		}
+		if (total == null && points) {
+			error = 'Please enter a total value.'
+			return
+		}
+		if (points < 0) {
+			error = 'Points cannot be negative.'
+			return
+		}
+		if (total < 0) {
+			error = 'Total cannot be negative.'
+			return
+		}
+		shown = false
+		console.log(points, total)
+		if (points == assignment.pointsOriginal && total == assignment.totalOriginal) {
+			assignment.edited = false
+		} else {
+			assignment.edited = true
+		}
+		assignment.points = points
+		assignment.total = total
+		parseData($session, null)
+		$session = $session
+	}
+</script>
+
+{#if shown}
+	<div class="background" on:click|self={cancel} transition:fade={{ duration: 200 }}>
+		<div
+			class="modal"
+			in:fly={{ y: -5, duration: 200 }}
+			out:fly|local={{ y: 5, duration: 200 }}
+		>
+			<h3>Edit Assignment</h3>
+			<div class="row info">
+				<p class="name">{assignment.Measure}</p>
+				<p class="date">{assignment.DueDate}</p>
+			</div>
+			<!-- <p class="type">{assignment.Type}</p>
+			<p class="date">{assignment.DueDate}</p> -->
+			<div class="row">
+				<input
+					class="points"
+					type="number"
+					min="0"
+					placeholder="Points"
+					bind:value={points}
+				/>
+				<input class="total" type="number" min="0" placeholder="Total" bind:value={total} />
+			</div>
+			{#if error}
+				<div class="error">{error}</div>
+			{/if}
+			<div class="row">
+				{#if assignment.fake}
+					<button on:click={del}>Delete</button>
+				{:else}
+					<button on:click={reset}>Reset</button>
+				{/if}
+				<button on:click={cancel}>Cancel</button>
+				<button on:click={save}>Save</button>
+			</div>
+		</div>
+	</div>
+{/if}
+
+<style lang="scss">
+	h3 {
+		margin: 0 0 $spacing-small 0;
+	}
+
+	.background {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: rgba(0, 0, 0, 0.4);
+		backdrop-filter: blur(3px);
+		display: flex;
+	}
+
+	.modal {
+		@include box;
+		margin: auto;
+		width: 500px;
+	}
+
+	input,
+	button {
+		width: 100%;
+	}
+
+	.row {
+		display: flex;
+		gap: $spacing-small;
+		margin-top: $spacing-small;
+		&.info {
+			margin: 0;
+		}
+	}
+
+	.error {
+		margin-top: $spacing-small;
+	}
+
+	.date {
+		margin-left: auto;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,187 @@
+<script context="module">
+	export async function load({ session }) {
+		if (!session.user) {
+			return {
+				status: 302,
+				redirect: '/login'
+			}
+		}
+		return {}
+	}
+</script>
+
+<script>
+	import { session } from '$app/stores'
+
+	let date = new Date()
+	let greeting = date.getHours() < 12 ? 'morning' : date.getHours() < 18 ? 'afternoon' : 'evening'
+</script>
+
+<svelte:head>
+	<title>StudentVue</title>
+</svelte:head>
+
+<div class="layout">
+	<h1>Good {greeting}, {$session.student.FormattedName.split(' ')[0]}!</h1>
+	<div class="average value">
+		<h1 style={$session.gradebook.averageStyle}>
+			{$session.gradebook.average}
+		</h1>
+		<div class="value-label">Average grade<br />&nbsp;</div>
+	</div>
+	<div class="improvement value">
+		<h1 style={$session.gradebook.week.averageStyle}>
+			{$session.gradebook.week.average}
+		</h1>
+		<div class="value-label">Average grade<br />this week</div>
+	</div>
+	<div class="week-assignments value">
+		<h1>{$session.gradebook.week.length}</h1>
+		<div class="value-label">
+			{$session.gradebook.week.length === 1 ? 'Assignment' : 'Assignments'}
+			<br />this week
+		</div>
+	</div>
+	<div class="days value">
+		<h1>
+			{$session.gradebook.days}
+		</h1>
+		<div class="value-label">
+			{$session.days === 1 ? 'Day' : 'Days'} left in
+			<br />{$session.gradebook.ReportingPeriod.GradePeriod}
+		</div>
+	</div>
+	<div class="grades">
+		<a class="link" sveltekit:prefetch href="/grades"><h2>Grades</h2></a>
+		<table class="grades-table">
+			{#each $session.gradebook.Courses.Course as course, index}
+				<tr>
+					<td class="course-name">
+						<a class="link" sveltekit:prefetch href={'/course/' + index}>
+							{course.Title}
+						</a>
+					</td>
+					<td class="course-grade" style={course.style}>
+						{course.Marks.Mark.CalculatedScoreString}
+					</td>
+					<td class="course-score" style={course.style}>{course.score}</td>
+				</tr>
+			{/each}
+		</table>
+	</div>
+	<div class="assignments">
+		<div class="assignments-scroll">
+			<a class="link" sveltekit:prefetch href="/assignments"><h2>Assignments</h2></a>
+			<table class="assignments-table">
+				{#each $session.gradebook.assignments as assignment}
+					{#if assignment.scorePercent >= 0}
+						<tr class={assignment.fake || assignment.edited ? 'fake' : ''}>
+							<td
+								class="assignment-name"
+								style={assignment.new ? 'font-weight: bold;' : ''}
+							>
+								{assignment.Measure}
+							</td>
+							<td class="assignment-score" style={assignment.style}>
+								{assignment.score}
+							</td>
+						</tr>
+					{/if}
+				{/each}
+			</table>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.layout {
+		display: grid;
+		height: 100%;
+		width: 100%;
+		gap: $spacing;
+		grid-template-columns: 1fr 1fr 1fr 1fr 2fr;
+		grid-template-rows: auto auto 1fr;
+	}
+
+	h1 {
+		margin-bottom: 0px;
+		grid-column: 1 / 5;
+	}
+
+	.value {
+		@include box;
+		aspect-ratio: 1;
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		min-width: 0;
+		min-height: 0;
+		overflow: hidden;
+		font-size: 0.9em;
+		padding: 0;
+		h1 {
+			margin-top: auto;
+			margin-bottom: $spacing-small;
+		}
+		div {
+			margin-bottom: auto;
+		}
+	}
+
+	.grades {
+		@include box;
+		grid-column: 1 / 5;
+	}
+
+	.assignments {
+		@include box;
+		padding: 0;
+		grid-column: 5;
+		grid-row: 1 / 4;
+		overflow: hidden;
+	}
+
+	.assignments-scroll {
+		height: 100%;
+		overflow-y: auto;
+		scrollbar-color: var(--bg-color-2-5) transparent;
+		padding: $spacing;
+	}
+
+	.grades-table {
+		height: calc(100% - 2 * $spacing);
+	}
+
+	.assignments-table {
+		table-layout: fixed;
+		width: 100%;
+	}
+
+	.assignments-table td {
+		padding-top: $spacing-small;
+		padding-bottom: $spacing-small;
+	}
+
+	.assignment-name {
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		width: 100%;
+	}
+
+	.assignment-score {
+		width: 80px;
+		text-align: right;
+		white-space: nowrap;
+		padding-left: 10px;
+	}
+
+	.course-grade {
+		padding: 0;
+		font-weight: bold;
+	}
+
+	.course-score {
+		text-align: right;
+	}
+</style>
